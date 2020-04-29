d7b1da82fb0bbde7ad7e27aaee94ee92d6798477
Matheus Ferro <matheusferroraimundo@gmail.com>
2020-04-29 02:05:30 -0300

    new

--- index.php
+++ index.php
@@ -1,17 +1,65 @@
-<?php
-
-    echo 'Ola';
-
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <header class="menu-principal">
+        
+    </header>
+
+    <section class="container-corpo">
+        <div class="page-left">
+            <div class="box-corpo-left">
+                <div class="box-modulo">
+                    <h1><a href="">Módulo de JavaScript</a></h1>
+                    <div class="box-nome-aula">
+                        <p><a href="">Aula 1 - Primeiro contato</a></p>
+                        <p><a href="">Aula 2 - Algoritmos</a></p>
+                        <p><a href="">Aula 3 - Variáveis</a></p>
+                        <p><a href="">Aula 4 - Funções</a></p>
+                        <p><a href="">Aula 5 - Laços</a></p>
+                    </div>
+                </div>
+                
+                <div class="box-modulo">
+                    <h1><a href="">Módulo de JavaScript</a></h1>
+                    <div class="box-nome-aula">
+                        <p><a href="">Aula 1 - Primeiro contato</a></p>
+                        <p><a href="">Aula 2 - Algoritmos</a></p>
+                        <p><a href="">Aula 3 - Variáveis</a></p>
+                        <p><a href="">Aula 4 - Funções</a></p>
+                        <p><a href="">Aula 5 - Laços</a></p>
+                    </div>
+                </div>
+
+                <div class="box-modulo">
+                    <h1><a href="">Módulo de JavaScript</a></h1>
+                    <div class="box-nome-aula">
+                        <p><a href="">Aula 1 - Primeiro contato</a></p>
+                        <p><a href="">Aula 2 - Algoritmos</a></p>
+                        <p><a href="">Aula 3 - Variáveis</a></p>
+                        <p><a href="">Aula 4 - Funções</a></p>
+                        <p><a href="">Aula 5 - Laços</a></p>
+                    </div>
+                </div>
+
+                
+            </div>
+        </div>
+
+        <div class="center-page">
+            <h1>Aula 1 - Primeiro contato</h1>
+            <div class="box-videoaula">
+
+            </div>
+        </div>
+    </section>
+
 </body>
+
 </html>

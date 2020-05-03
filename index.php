b0c1b379d80770a2e849ecb019d7f18bf3b23f6f
Matheus Ferro <matheusferroraimundo@gmail.com>
2020-05-03 02:37:45 -0300

    030520

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
+    <title>EAD</title>
 </head>
 
 <body>
@@ -27,33 +27,10 @@
                         <p><a href="">Aula 4 - Funções</a></p>
                         <p><a href="">Aula 5 - Laços</a></p>
                     </div>
-                </div>
-                
-                <div class="box-modulo">
-                    <h1><a href="">Módulo de JavaScript</a></h1>
-                    <div class="box-nome-aula">
-                        <p><a href="">Aula 1 - Primeiro contato</a></p>
-                        <p><a href="">Aula 2 - Algoritmos</a></p>
-                        <p><a href="">Aula 3 - Variáveis</a></p>
-                        <p><a href="">Aula 4 - Funções</a></p>
-                        <p><a href="">Aula 5 - Laços</a></p>
-                    </div>
-                </div>
-
-                <div class="box-modulo">
-                    <h1><a href="">Módulo de JavaScript</a></h1>
-                    <div class="box-nome-aula">
-                        <p><a href="">Aula 1 - Primeiro contato</a></p>
-                        <p><a href="">Aula 2 - Algoritmos</a></p>
-                        <p><a href="">Aula 3 - Variáveis</a></p>
-                        <p><a href="">Aula 4 - Funções</a></p>
-                        <p><a href="">Aula 5 - Laços</a></p>
-                    </div>
-                </div>
-
-                
+                </div>      
             </div>
         </div>
+        
 
         <div class="center-page">
             <h1>Aula 1 - Primeiro contato</h1>
